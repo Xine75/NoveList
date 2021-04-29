0182ad647ae9681d3b36c9230b96eda7b064cd30
Xine <cjdoza@gmail.com>
2021-04-29 14:15:57 -0500

    setting up fullstack users

---
+++ NoveList/Controllers/UserProfileController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NoveList.Models;
+using NoveList.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoveList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        //GetAll
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userProfileRepository.GetAll());
+        }
+        //GetById
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _userProfileRepository.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        //GetUserByFirebaseId
+        [HttpGet("login/{firebaseUserId}")]
+        public IActionResult GetUserProfile(string firebaseId)
+        {
+            return Ok(_userProfileRepository.GetByFirebaseId(firebaseId));
+        }
+        //Add UserProfile
+        [HttpPost]
+        public IActionResult Post(UserProfile user)
+        {
+            _userProfileRepository.Add(user);
+            return CreatedAtAction("Get", new { id = user.Id }, user);
+        }
+        //Edit/Update UserProfile
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserProfile user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+            _userProfileRepository.Update(user);
+            return NoContent();
+        }
+        //Delete a UserProfile
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userProfileRepository.Delete(id);
+            return NoContent();
+        }
+        //Get a single user with all their books
+        [HttpGet("GetUserProfileByIdWithBooks/{id}")]
+        public IActionResult GetUserProfileByIdWithPosts(int id)
+        {
+            var user = _userProfileRepository.GetUserProfileByIdWithBooks(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+    }
+}
