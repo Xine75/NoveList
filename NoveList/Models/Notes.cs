14a90c899e6f536ec5d7487a8af647b21b59b7c6
Xine <cjdoza@gmail.com>
2021-04-28 21:20:09 -0500

    models

---
+++ NoveList/Models/Notes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoveList.Models
+{
+    public class Notes
+    {
+        public int id { get; set; }
+        public int PageNum { get; set; }
+        public string Content { get; set; }
+        public int BookId { get; set; }
+    }
+}
