6b0f4d60343383354b146653a12f7031c068406f
Xine <cjdoza@gmail.com>
2021-04-30 16:05:42 -0500

    set up querystring

--- NoveList/Program.cs
+++ NoveList/Program.cs
@@ -5,16 +5,35 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace NoveList
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await ProcessRepositories();
         }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var stringTask = client.GetStringAsync("https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=AIzaSyAWa0D5qNDRqhiLmfU5nE3w7X5ivwP9MZ8");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+
+
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -22,5 +41,7 @@ namespace NoveList
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+      
     }
 }
