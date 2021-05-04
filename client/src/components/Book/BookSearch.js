7f4b729aff030a52e8fca42d93befb8a9ea98c03
Xine <cjdoza@gmail.com>
2021-05-04 12:05:46 -0500

    useEffect for search

--- client/src/components/Book/BookSearch.js
+++ client/src/components/Book/BookSearch.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from 'react-router-dom';
 import { SearchContext } from "../Providers/SearchProvider";
 import { Button } from 'react-bootstrap'
@@ -9,19 +9,31 @@ export const GoogleSearch = () => {
     const { Search } = useContext(SearchContext);
     const history = useHistory();
 
-    const searchSubmit = (e) => {
-        e.preventDefault();
-        Search(searchTerms)
-            .then(() => history.push(`/search/${searchTerms}`))
-            .then(() => setSearchResults(e.target.value))
-    }
+    // const searchSubmit = (e) => {
+    //     e.preventDefault();
+    //     Search(searchTerms)
+    //         .then(() => history.push(`/search/${searchTerms}`))
+    //         .then(() => setSearchResults(e.target.value))
+    // }
+
+    useEffect(() => {
+        Search(searchTerms);
+    }, []);
+
     return (
         <>
             Search for a Book:
+            <div className="instrument-search__form">
+                <input id="search" value={criterion} onChange={e => setSearchTerms(e.target.value)} />
+                <Button onClick={() => searchInstruments(searchTerms)}>Search</Button>
+            </div>
+
+
+            {/*             
             <input type="text" className="input--wide"
 
                 placeholder="Search by author or title" />
-            <Button onClick={searchSubmit}>Search</Button>
+            <Button onClick={searchSubmit}>Search</Button> */}
         </>
     )
 }
