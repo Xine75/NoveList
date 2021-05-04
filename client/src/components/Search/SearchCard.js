ef9f1f1112366f4bca9086170755c0f8879b901b
Xine <cjdoza@gmail.com>
2021-05-04 17:53:23 -0500

    search results showing with images

--- client/src/components/Search/SearchCard.js
+++ client/src/components/Search/SearchCard.js
@@ -5,20 +5,20 @@ import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import Button from "react-bootstrap/Button"
+import "./Search.css"
 
 //SearchCard will take care of the details of each novel:
 //id, title, image, author, summary and link to add novel to collection
 
 export const SearchCard = ({ searchResult }) => {
-    debugger
     return (
         <>
             <Card className="searchResult_card" border="success">
                 <Card.Body>
-                    <Card.Img className="searchResult__card__image" src={searchResult.imageURL} />
+                    <Card.Img className="searchResult__card__image" src={searchResult.thumbnail} />
                     <Card.Title className="searchResult_title">
                         {searchResult.title}
-                        <div className="searchResult__author">{searchResult.authors.ToString()}</div>
+                        <div className="searchResult__author">{searchResult.authors}</div>
                         <div className="searchResult__summary">{searchResult.textSnippet}</div>
                     </Card.Title>
                 </Card.Body>
