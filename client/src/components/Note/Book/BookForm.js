1ab1566af7d6e304532bcdeaaf06f8e804447425
Xine <cjdoza@gmail.com>
2021-05-05 15:32:40 -0500

    modal test run

--- client/src/components/Note/Book/BookForm.js
+++ client/src/components/Note/Book/BookForm.js
@@ -36,13 +36,49 @@ export const BookForm = (props) => {
         props.onHide()
     };
 
-    //------------setting up Modal --------------------
-
-
-
+    //--------------Setting up modal------------------
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     //---------------JSX ADD BOOK Modal-----------------------
+    return (
+        <>
 
+            <Modal id="bootstrap"
+                {...props}
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton id="bootstrap">
+                    <Modal.Title>Add Novel to My Library</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {/* import details of chosen book */}
+
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="shelfId">Add To Shelf:</label>
+                            <select value={book.shelfId} name="shelfId" id="shelfId" className="form-control" onChange={handleControlledInputChange}>
+                                <option value="0">Choose a shelf</option>
+                                <option value="1">Currently Reading</option>
+                                <option value="2">Book Club</option>
+                                <option value="3">Mystery</option>
+                                <option value="4">Historical Fiction</option>
+                                <option value="5">Beach Read</option>
+                            </select>
+                        </div>
+                    </fieldset>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="primary" onClick={addBook}>Save</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
 
 
 
