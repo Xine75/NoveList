51f13fee64a26e866f7f931f79800e3425612826
Xine <cjdoza@gmail.com>
2021-05-07 15:12:27 -0500

    add note modal

--- client/src/components/Note/NoteList.js
+++ client/src/components/Note/NoteList.js
@@ -9,10 +9,44 @@ export const NoteList = () => {
     const history = useHistory();
     const bookId = useParams().id;
 
+    //--------------Setting up modal------------------
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    //-------------------Setting State----------------------
+    const { addNote } = useContext(NoteContext);
+    const [note, setNote] = useState({
+        id: 0,
+        pageNum: 0,
+        content: "",
+        bookId: bookId
+    })
+
+    //-------------Saving User Input-------------
+    const handleControlledInputChange = (e) => {
+        const newNote = { ...book }
+        newBook[e.target.id] = e.target.value
+        setNote(newNote)
+    };
+
+    //--------------Saving New Note upon Click event-----------------
+    const handleClickSaveNote = (e) => {
+        e.preventDefault()
+        const newNote = { ...note }
+        addNote(newNote)
+            .then(() => history.push(`/book/${bookId}`))
+
+    }
+    //------------------Get Notes by Id------------------------
     useEffect(() => {
-        getNotesByBookId(bookId);
+        getNotesByBookId(bookId)
+            .then(note => {
+                setNote(note)
+            })
     }, []);
 
+    //------------------JSX for NoteList ----------------------------
     return (
         <>
             <h4 className="notes__header">Notes</h4>
@@ -27,10 +61,42 @@ export const NoteList = () => {
                 }
             </div>
             <div>
-                <Button className="notes__btn" size="sm" variant="info" onClick={() => { history.push("api/note") }}>
+                <Button className="notes__btn" size="sm" variant="info" onClick={handleShow}>
                     Add a Note
                 </Button>
             </div>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>What are you thinking?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="name">Page?</label>
+                            <input type="text" id="pageNum" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Plant name" value={plant.name} />
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="name">Thoughts?</label>
+                            <input type="textarea" rows="8" id="type" onChange={handleControlledInputChange} required className="form-control" placeholder="Scientific or common" value={plant.type} />
+                        </div>
+                    </fieldset>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="primary" onClick={handleClickSaveNote}>Add</Button>
+                </Modal.Footer>
+            </Modal>
 
         </>
     );
