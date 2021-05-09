668d6169df1b71c0c0995d7c5939ae54e2cc9d0b
Xine <cjdoza@gmail.com>
2021-05-09 07:36:17 -0500

    starting friends

--- NoveList/Models/Friend.cs
+++ NoveList/Models/Friend.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 
 namespace NoveList.Models
 {
-    public class Shelf
+    public class Friend
     {
-        public int? Id { get; set; }
-        public string Name { get; set; }
-        public int UserId { get; set; }
+        public int Id { get; set; }
+        public int Friend1 { get; set; }
+        public int Friend2 { get; set; }
     }
 }
---
+++ NoveList/Repositories/ShelfRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoveList.Repositories
+{
+    public class ShelfRepository
+    {
+        //get all shelves
+        //add shelf
+
+    }
+}
